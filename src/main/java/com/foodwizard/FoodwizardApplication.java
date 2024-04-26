f5b882d3a4ee8f81a016fcaffc17015590baac4c
mskim <wemb@wembui-Macmini.local>
2024-04-26 13:16:49 +0900

    first commit

---
+++ src/main/java/com/foodwizard/FoodwizardApplication.java
@@ -0,0 +1,13 @@
+package com.foodwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodwizardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodwizard/FoodwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodwizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
