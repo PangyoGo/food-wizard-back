4548282f9b3de4b2cb516b84b8a47178da75c3e6
mskim <wemb@wembui-Macmini.local>
2024-06-27 08:34:16 +0900

    feat: 음식 메뉴 한가지만 추천하도록 변경

--- src/main/java/com/foodwizard/chatgpt/controller/ChatgptController.java
+++ src/main/java/com/foodwizard/chatgpt/controller/ChatgptController.java
@@ -1,16 +1,15 @@
 package com.foodwizard.chatgpt.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foodwizard.chatgpt.controller.dto.FoodResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,9 +27,12 @@ public class ChatgptController {
     private final OpenAiApi openAiApi;
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/chat")
-    public List<String> chat(@RequestParam String prompt) {
-        return getFoodList(getChatCompletion(prompt));
+    public FoodResponse chat(@RequestParam String prompt) {
+        return FoodResponse.builder()
+                .food(getFoodList(getChatCompletion(prompt)))
+                .build();
     }
 
     private ResponseEntity<OpenAiApi.ChatCompletion> getChatCompletion(String prompt) {
@@ -39,21 +41,18 @@ public class ChatgptController {
                 .append(prompt)
                 .append("이다.\n")
                 .append("나의 기분에 어울리는 점심 식사를 하려고 한다.")
-                .append("나의 기분에 맞는 점심 식사 메뉴로 적절한 음식 종류를 3가지 추천해줘.")
+                .append("나의 기분에 맞는 점심 식사 메뉴로 적절한 음식 종류를 1가지 추천해줘.")
                 .append("디저트와 관련 된 음식은 제외한다.")
-                .append("결과는 [음식;음식;음식3] 형태로 나와야 한다.");
+                .append("결과는 [음식] 형태로 나와야 한다.");
         OpenAiApi.ChatCompletionMessage message = new OpenAiApi.ChatCompletionMessage(sb.toString(), OpenAiApi.ChatCompletionMessage.Role.USER);
         OpenAiApi.ChatCompletionRequest request = new OpenAiApi.ChatCompletionRequest(Arrays.asList(message), model, 0.7F);
 
         return openAiApi.chatCompletionEntity(request);
     }
 
-    private List<String> getFoodList(ResponseEntity<OpenAiApi.ChatCompletion> response) {
-        List<String> foodList = Arrays.stream(
-                response.getBody().choices().get(0).message().content()
-                        .replace("[","")
-                        .replace("]", "").split(";")
-                ).map(String::trim).collect(Collectors.toList());
-        return foodList;
+    private String getFoodList(ResponseEntity<OpenAiApi.ChatCompletion> response) {
+        return response.getBody().choices().get(0).message().content()
+                .replace("[","")
+                .replace("]", "");
     }
 }
---
+++ src/main/java/com/foodwizard/chatgpt/controller/dto/FoodResponse.java
@@ -0,0 +1,6 @@
+package com.foodwizard.chatgpt.controller.dto;
+
+import lombok.Builder;
+
+@Builder
+public record FoodResponse(String food) { }
